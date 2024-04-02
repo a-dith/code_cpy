ea2a3aeedd7856763cf2abda02b56eac69fcacb5
Adith pp <136182506+a-dith@users.noreply.github.com>
2024-04-02 12:25:34 +0530

    Update code_cpy.sh
    commented out output to clip board and 
    using xdotool to directly type the output

--- code_cpy.sh
+++ code_cpy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 file=$1
-line=$(cat /home/e15/scripts/$1 | dmenu -p "select line" -i -l 20 -nb black -nf white -sb dodgerblue -sf white -fn 'Monospace-12')
+line=$(cat /home/e15/scripts/$1 | dmenu -c -p "select line" -i -l 20 -nb black -nf white -sb dodgerblue -sf white -fn 'Monospace-12')
 selected_field=$(echo "$line" | awk -F'~' '{print $2}')
 formatted_field=$(echo -n "$selected_field" | sed 's/^[[:space:]]*//' )
-echo -n "$formatted_field" | xclip -selection c
+# echo -n "$formatted_field" | xclip -selection c
+xdotool type "$formatted_field"
 
 #    Reads the contents of the file /home/e15/scripts/1.txt using cat.
 #    Pipes the output of cat to dmenu to display a menu for selecting a line. The selected line is stored in the line variable.
@@ -12,3 +13,4 @@ echo -n "$formatted_field" | xclip -selection c
 #    uses the sed command with a regular expression pattern s/^[[:space:]]*// to remove any leading whitespace characters ([[:space:]]) from the selected_field variable. 
 #    use echo -n flag to remove newline \n character
 #    Pipes the contents of formatted_field to xclip to copy it to the clipboard using the c selection.
+#    using xdotool type the output
